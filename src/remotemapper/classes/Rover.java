55f67a39463b3de01071e6c2005f1568651d4b17
Robert Hutter <rohu7@DESKTOP-C5J9EGG.t.hu>
2018-06-29 19:33:20 +0100

    Countless bug fixes. GUI improvement. Framework improvements.

--- src/remotemapper/classes/Rover.java
+++ src/remotemapper/classes/Rover.java
@@ -14,7 +14,7 @@ public class Rover
     protected int x, y, width, length, height;
     protected Angle direction;
     
-    public Rover (int x, int y, int dir, int width, int length)
+    public Rover (int x, int y, float dir, int width, int length)
     {
         this.x = x;
         this.y = y;
@@ -23,7 +23,7 @@ public class Rover
         this.length = length;
     }
     
-    public Rover (int x, int y, int dir, int width, int length, int height)
+    public Rover (int x, int y, float dir, int width, int length, int height)
     {
         this.x = x;
         this.y = y;
@@ -31,5 +31,55 @@ public class Rover
         this.width = width;
         this.length = length;
         this.height = height;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    
+    public void setDirection (float dir)
+    {
+        this.direction.setAngel(dir);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+    
+    public float getDirection ()
+    {
+        return this.direction.size;
     }
 }
