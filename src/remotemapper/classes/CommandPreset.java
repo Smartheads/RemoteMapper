98a10d0fd9fb2d5755ff88a8cc4c1d8433c76cab
Robert Hutter <rohu7@DESKTOP-C5J9EGG.t.hu>
2018-07-02 18:01:56 +0100

    Optimized code. Finished XML implementation.

--- src/remotemapper/classes/CommandPreset.java
+++ src/remotemapper/classes/CommandPreset.java
@@ -6,9 +6,13 @@
 
 package remotemapper.classes;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
@@ -16,32 +20,81 @@ import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
+import remotemapper.exceptions.LoadWorkspaceException;
 
 /**
  * A Class for holding command presets.
  * 
- * @author rohu7
+ * @author Robert Hutter
  */
 public class CommandPreset
 {
+
+    /**
+     * String command containing the presets command.
+     */
     protected String command;
+
+    /**
+     * String containing the presets name.
+     */
     protected String name;
+
+    /**
+     * String containing the presets short description.
+     */
     protected String description;
     
-    public CommandPreset (String name, String command, String description)
+    /**
+     * Integer that contains the presets id. Constant.
+     */
+    private final int id;
+    
+    /**
+     *
+     * @param name Presets name
+     * @param command Presets command
+     * @param description Presets short description
+     * @param id Presets ID number
+     */
+    public CommandPreset (String name, String command, String description, int id)
     {
         this.name = name;
         this.command = command;
         this.description = description;
+        this.id = id;
     }
     
+    /**
+     *
+     * @param name Presets name
+     * @param id Presets ID number
+     */
+    public CommandPreset (String name, int id)
+    {
+        this.name = name;
+        this.command = "";
+        this.description = "";
+        this.id = id;
+    }
+    
+    /**
+     * Creates an empty preset. ID initialized to 0.
+     */
     public CommandPreset ()
     {
         this.command = "";
         this.name = "";
         this.description = "";
+        this.id = 0;
     }
     
+    /**
+     * Save preset to specified output file in XML format.
+     * 
+     * @param out File to save to
+     * @throws IOException
+     */
     public void saveToFile (File out) throws IOException
     {
         /*
@@ -49,6 +102,9 @@ public class CommandPreset
         * if is, append / overwrite command preset,
         * if not, save command preset in a new XML.
         */
+        
+        FileOutputStream fos = null;
+        
         try
         {
             SAXBuilder saxBuilder = new SAXBuilder();
@@ -57,13 +113,14 @@ public class CommandPreset
             Element root = doc.getRootElement();
             List<Element> subElements = root.getChildren();
             
-            // Try to find prseset and get update its element
+            // Try to find preset and get update its element
             Element preset = null;
             for (int i = 0; i < subElements.size(); i++)
             {
-                if (subElements.get(i).getAttribute("name").equals(this.name))
+                if (subElements.get(i).getAttributeValue("id").equals(""+this.id))
                 {
                     preset = subElements.get(i);
+                    break;
                 }
             }
             
@@ -72,15 +129,12 @@ public class CommandPreset
             {
                 // Update the element's data
                 List<Element> data = preset.getChildren();
+                preset.setAttribute("name", this.name);
                 
                 for (int i = 0; i < data.size(); i++)
                 {
                     switch (data.get(i).getName())
-                    {
-                        case "name":
-                            data.get(i).setText(this.name);
-                            break;
-                            
+                    {                          
                         case "command":
                             data.get(i).setText(this.command);
                             break;
@@ -96,6 +150,7 @@ public class CommandPreset
                 // Append the new preset
                 preset = new Element("preset");
                 preset.setAttribute(new Attribute("name", this.name));
+                preset.setAttribute(new Attribute("id", ""+this.id));
                 
                 Element cmd = new Element("command");
                 cmd.setText(this.command);
@@ -110,54 +165,167 @@ public class CommandPreset
             }
             
             // Output to file
+            fos = new FileOutputStream(out);
+            
             XMLOutputter XMLOutput = new XMLOutputter();
             XMLOutput.setFormat(Format.getPrettyFormat());
-            XMLOutput.output(root, new FileWriter(out));
+            XMLOutput.output(root, fos);
+            
+            fos.flush();
+            fos.close();
         }
-        catch (JDOMException jdome)
+        catch (JDOMException | FileNotFoundException | NullPointerException jdome)
         {
-            // Write new XML file
-            Element presetsElement = new Element("command presets");
-            Document doc = new Document (presetsElement);
+            Logger.getLogger(CommandPreset.class.getName()).log(Level.WARNING, null, jdome);
             
-            Element preset = new Element("preset");
-            preset.setAttribute(new Attribute("name", this.name));
-            
-            Element cmd = new Element("command");
-            cmd.setText(this.command);
-            
-            Element desc = new Element("description");
-            desc.setText(this.description);
-            
-            preset.addContent(cmd);
-            preset.addContent(desc);
-            
-            XMLOutputter XMLOutput = new XMLOutputter();
-            XMLOutput.setFormat(Format.getPrettyFormat());
-            XMLOutput.output(doc, new FileWriter(out));
+            try 
+            {
+                // Write new XML file
+                Element presetsElement = new Element("command_presets");
+                Document doc = new Document (presetsElement);
+                
+                Element preset = new Element("preset");
+                preset.setAttribute(new Attribute("name", this.name));
+                preset.setAttribute(new Attribute("id", ""+this.id));
+                
+                Element cmd = new Element("command");
+                cmd.setText(this.command);
+                
+                Element desc = new Element("description");
+                desc.setText(this.description);
+                
+                preset.addContent(cmd);
+                preset.addContent(desc);
+                
+                presetsElement.addContent(preset);
+                
+                fos = new FileOutputStream(out);
+                
+                XMLOutputter XMLOutput = new XMLOutputter();
+                XMLOutput.setFormat(Format.getPrettyFormat());
+                XMLOutput.output(doc, fos);
+                
+                fos.flush();
+                fos.close();
+            } 
+            catch (IOException ex) 
+            {
+                Logger.getLogger(CommandPreset.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            finally
+            {
+                try
+                {
+                    fos.close();
+                }
+                catch (IOException e)
+                {
+                    Logger.getLogger(CommandPreset.class.getName()).log(Level.SEVERE, null, e);
+                }
+            }
         }
     }
+    
+    /**
+     * Load all presets form a file saved with the XML format.
+     * 
+     * @param file File to read data from.
+     * @return An arrayList containing the presets.
+     * @throws remotemapper.exceptions.LoadWorkspaceException
+     */
+    public static ArrayList<CommandPreset> loadPresets (File file) throws LoadWorkspaceException
+    {
+        try {
+            SAXBuilder sax = new SAXBuilder();
+            Document doc = sax.build(file);
+            Element root = doc.getRootElement();
+            
+            ArrayList<CommandPreset> cps = new ArrayList();
+            List<Element> presets = root.getChildren();
+            
+            for (int i = 0; i < presets.size(); i++)
+            {
+                cps.add(new CommandPreset(presets.get(i).getAttributeValue("name"), i));
+                
+                for (Element d : presets.get(i).getChildren())
+                {
+                    switch (d.getName())
+                    {
+                        case "command":
+                            cps.get(i).setCommand(d.getText());
+                            break;
+                            
+                        case "description":
+                            cps.get(i).setDescription(d.getText());
+                            break;
+                    }
+                }
+            }
+            
+            return cps;
+        }
+        catch (JDOMException | IOException ex)
+        {
+            Logger.getLogger(CommandPreset.class.getName()).log(Level.SEVERE, null, ex);
+            
+            // Throw unified exception to let processing know, load failed
+            throw new LoadWorkspaceException();
+        }
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public int getId ()
+    {
+       return id; 
+    }
 
+    /**
+     *
+     * @return
+     */
     public String getCommand() {
         return command;
     }
 
+    /**
+     *
+     * @param command
+     */
     public void setCommand(String command) {
         this.command = command;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     *
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     *
+     * @param description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
