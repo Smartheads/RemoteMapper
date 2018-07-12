f6177a8cbdd604e5129f99fd6ea0eca665947338
Robert Hutter <rohu7@DESKTOP-C5J9EGG.t.hu>
2018-07-12 15:44:08 +0100

    Updated GUI

--- src/remotemapper/MapPreviewer.java
+++ src/remotemapper/MapPreviewer.java
@@ -82,11 +82,11 @@ public class MapPreviewer extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 928, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 649, Short.MAX_VALUE)
         );
 
         pack();
